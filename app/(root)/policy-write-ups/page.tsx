accaf72d59c2030a05ed2fdfd8e77efb6bfdcaf4
Van Truong <43657607+vtruo009@users.noreply.github.com>
2025-03-17 19:31:26 -0700

    set up routes

---
+++ app/(root)/policy-write-ups/page.tsx
@@ -0,0 +1,9 @@
+const PolicyWriteups = () => {
+	return (
+		<div>
+			<h1>This is where the policy write-ups are</h1>
+		</div>
+	);
+};
+
+export default PolicyWriteups;
---
+++ app/(root)/videos/page.tsx
@@ -0,0 +1,9 @@
+const Videos = () => {
+	return (
+		<div>
+			<h1>This is short form videos page</h1>
+		</div>
+	);
+};
+
+export default Videos;
