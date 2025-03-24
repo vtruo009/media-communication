f22bbd04b16d51c0cde55ca5e9a3c86b6934384f
Van Truong <43657607+vtruo009@users.noreply.github.com>
2025-03-24 11:27:58 -0700

    change env DATABASE_URL

--- lib/database.ts
+++ lib/database.ts
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL || '');
+const sql = neon(process.env.DATABASE_URL || '');
 
 export const getAllWriteups = async () => {
 	try {
