938b3cf9b387f47a294d0f75f5706d63902deadf
Van Truong <43657607+vtruo009@users.noreply.github.com>
2025-03-25 15:27:21 -0700

    pagination done

--- app/(root)/policy-write-ups/page.tsx
+++ app/(root)/policy-write-ups/page.tsx
@@ -1,15 +1,31 @@
+import { PaginationWithLinks } from '@/components/ui/pagination-with-links';
 import Writeup from '@/components/Writeup';
-import { getAllWriteups } from '@/lib/database';
-import { WriteUp } from '@/lib/mixin';
+import { getWriteupCount, getWriteups } from '@/lib/database';
 
-const PolicyWriteups = async () => {
+const PolicyWriteups = async ({
+	searchParams,
+}: {
+	searchParams: { page: string };
+}) => {
 	try {
-		const writeups = (await getAllWriteups()) as WriteUp[];
+		const { page } = await searchParams;
+		const size = 10;
+
+		const count = await getWriteupCount();
+		const writeups = await getWriteups(Number(page), size);
+
 		return (
 			<div className='py-12 px-52'>
 				{writeups.map((writeup) => (
 					<Writeup key={writeup.id} {...writeup} />
 				))}
+				<div className='py-8'>
+					<PaginationWithLinks
+						totalCount={count}
+						pageSize={size}
+						page={Number(page)}
+					/>
+				</div>
 			</div>
 		);
 	} catch (error) {
