659da2ed931fa1ba4733a03b9ceb199d4f02a8f1
Van Truong <43657607+vtruo009@users.noreply.github.com>
2025-04-18 16:03:59 -0700

    move FAQs to constants.ts

--- app/(root)/getting-started/page.tsx
+++ app/(root)/getting-started/page.tsx
@@ -5,7 +5,7 @@ import {
 	AccordionItem,
 	AccordionTrigger,
 } from '@/components/ui/accordion';
-import { INSTRUCTIONS } from '@/lib/constants';
+import { INSTRUCTIONS, FAQS } from '@/lib/constants';
 import { DynamicIcon } from 'lucide-react/dynamic';
 import dynamicIconImports from 'lucide-react/dynamicIconImports';
 
@@ -35,30 +35,12 @@ const Instruction = ({ label, iconName, instruction }: InstructionProps) => {
 	);
 };
 
-// TODO: Move to constants.ts
-const faqs = [
-	{
-		question: 'What is the purpose of this website?',
-		answer:
-			'To provide a platform for users who would like to make a difference in the PUSD.',
-	},
-	{
-		question: 'How long do I have for each call?',
-		answer: 'Keep it short. A call can be under a minute.',
-	},
-	{
-		question: 'What do I say?',
-		answer:
-			'We provide a script on the overview page. Stick to the script or feel free to improvise!',
-	},
-];
-
 const FAQ = () => {
 	return (
 		<>
 			<h2 className='text-3xl font-bold'>FAQs</h2>
 			<Accordion type='single' collapsible className='w-[50vw] h-fit'>
-				{faqs.map((faq, i) => (
+				{FAQS.map((faq, i) => (
 					<AccordionItem key={`faq-${i}`} value={`faq-${i}`}>
 						<AccordionTrigger>{faq.question}</AccordionTrigger>
 						<AccordionContent className='data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp'>
