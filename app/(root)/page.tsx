fd72bdce598e8b66b53f18976405da96ae657e3c
Van Truong <43657607+vtruo009@users.noreply.github.com>
2025-04-18 22:55:27 -0700

    test polling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import TakeAction from '@/components/TakeAction';
 
+import Poll from '@/components/Poll';
+
 const Home = () => {
 	return (
 		<div>
 			<h1>This is home page</h1>
 			<TakeAction message="Let's make a difference in education." />
+			{/* <div data-tf-live='01JS67N8BTHVFCSDVBDRWHD39Y' data-tf-loading></div>
+			<script src='//embed.typeform.com/next/embed.js'></script> */}
+			<Poll />
 		</div>
 	);
 };
---
+++ components/Poll.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { Widget } from '@typeform/embed-react';
+import React from 'react';
+
+const Poll = () => {
+	return <Widget id='nkprjDDg' className='h-screen' />;
+};
+
+export default Poll;
